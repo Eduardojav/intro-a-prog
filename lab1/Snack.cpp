0670d88b36c23e9bcc58feb6709f5bbe4127ce44
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-09-20 22:11:36 -0500

    Update Snack.cpp

--- lab1/Snack.cpp
+++ lab1/Snack.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main (){
 
-int d,e;
-double g;
-double b[]={4.00,4.50,5.00,2.00,1.50};
-cin>>d;cin>>e;
-g=b[d-1]*e;
-cout<<"Total: R$ ";
-printf("%.2f\n", g);
+  int d,e;
+  double g;
+  double b[]={4.00,4.50,5.00,2.00,1.50};
+  cin>>d;cin>>e;
+  g=b[d-1]*e;
+  cout<<"Total: R$ ";
+  printf("%.2f\n", g);
 
 return 0;
 }
