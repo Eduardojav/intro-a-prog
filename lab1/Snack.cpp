3560ff47fbb42cade1fe8a09fab10fb23f928441
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-09-20 22:11:23 -0500

    Update Snack.cpp

--- lab1/Snack.cpp
+++ lab1/Snack.cpp
@@ -5,12 +5,9 @@ int main (){
 
 int d,e;
 double g;
-double f=0.01;
 double b[]={4.00,4.50,5.00,2.00,1.50};
 cin>>d;cin>>e;
 g=b[d-1]*e;
-g=g-f;
-g=g+0.01;
 cout<<"Total: R$ ";
 printf("%.2f\n", g);
 
