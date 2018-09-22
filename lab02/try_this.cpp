8f685ade73032e9d3400af102eb687c68645b257
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-09-22 16:54:31 -0500

    Create try_this.cpp

---
+++ lab02/try_this.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ( )
+{
+
+for ( int i=97 ; i <123; ++i ) {
+cout <<char(i)<<"  "<< i << endl;
+}
+for(int i=65; i<90;i++){
+	cout <<char(i)<<"   "<< i << endl;
+}
+for(int i=0; i<9;i++){
+	cout <<char(i)<<"   "<< i << endl;}
+return 0;}
