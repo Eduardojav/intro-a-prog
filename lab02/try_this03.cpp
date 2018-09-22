7af0bfb04679a65c525ceb3579c3c28d434e542d
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-09-22 16:54:10 -0500

    Update try_this03.cpp

--- lab02/try_this03.cpp
+++ lab02/try_this03.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main ( )
 {
 
-for ( int i=0 ; i <100; ++i ) {
-cout <<char(i)<<"   "<< i << endl;
+for ( int i=97 ; i <123; ++i ) {
+cout <<char(i)<<"  "<< i << endl;
 }
 }
