9c52c0782ad40f0b13874b6f5961b748e1598b76
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-09-29 13:28:52 -0500

    Create try_this_07.cpp

---
+++ lab02/try_this_07.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+#include <vector>
+using namespace std;
+
+int main(){
+string temp ;
+while ( cin >>temp ){
+    if(temp=="SHIT")
+        temp="BIIP";
+    cout<<" "<<temp<<" ";}
+    return 0;
+    }
