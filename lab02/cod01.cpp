3484259e47cb6795bb0b45a50b3cdd8678129b87
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-09-29 00:01:58 -0500

    Create cod01.cpp

---
+++ lab02/cod01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double d=0;
+    while(cin>>d){
+        int i = d;// si se puede pero pierde o no da la informacion que comumente se quiere dar, por lo que se pierde datos
+        char c= i;
+        int i2 =c;
+        cout <<"d= "<<d<<endl
+        <<"i= "<<i<<endl
+        <<"i2= "<<i2<<endl
+        <<"char= "<<c<<endl;
+    }
+    double x=2.7;
+    int y = x;
+    int a =1000;
+    char b= a;
+}
