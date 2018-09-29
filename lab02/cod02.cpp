72fb52dd43082122aa3941560fa6555f2e6399a2
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-09-29 09:45:00 -0500

    Create cod02.cpp

---
+++ lab02/cod02.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double d=2.5;
+        int i = 2;// si se puede pero pierde o no da la informacion que comumente se quiere dar, por lo que se pierde datos
+        double d2=d/i;
+        int i2 =d/i;
+        cout <<"d= "<<d<<endl
+        <<"i= "<<i<<endl
+        <<"d2= "<<d2<<endl
+        <<"i2= "<<i2<<endl;
+    double dc ;
+    cin >> dc ;
+    double df = 9.0/5 * dc +32;
+    cout <<"df= "<<df<<endl;
+    cin>>i2;
+}
