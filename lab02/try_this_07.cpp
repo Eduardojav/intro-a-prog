0ea619472afcdfde968f908a781188644affab36
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-09-29 15:23:44 -0500

    Update try_this_07.cpp

--- lab02/try_this_07.cpp
+++ lab02/try_this_07.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 int main(){
 string temp ;
+string words;
+vector<string> cosas;
+/*for(int i=0;i<5;++i){
+	cin>>words;*/
+cosas.push_back ("puto") ;
+cosas.push_back ("vieja") ;
+cosas.push_back ("shit") ;
+
 while ( cin >>temp ){
-    if(temp=="SHIT")
-        temp="BIIP";
-    cout<<" "<<temp<<" ";}
+	for ( int i =0;i<cosas.size ( ); ++i )
+    	if(temp==cosas[i])
+        	temp="pii";
+    	cout<<" "<<temp<<" ";}
     return 0;
     }
