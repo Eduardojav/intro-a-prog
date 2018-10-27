122e75bcecf03a2d7212eb631e3b8383aa50c430
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-10-27 14:44:01 -0500

    Update 3.cpp

--- resolucion/3.cpp
+++ resolucion/3.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include <math.h>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
-void mod(vector<string> &moda,int mx=1,int cont=0,int elemento=0){
+int mod(vector<int> &moda,int mx=1,int cont=0,int elemento=0){
 	for(int i=0;i<moda.size()-1;i++){
         for(int j=i+1;j<moda.size();j++){
             if(moda[i]==moda[j]){
@@ -13,10 +15,22 @@ void mod(vector<string> &moda,int mx=1,int cont=0,int elemento=0){
             mx=cont;
             elemento=i;}
         cont=0;}
-    cout<<moda[elemento]<<endl;
+    for(int i=0;i<moda.size();i++){
+    	cout<<moda[i]<<" ; ";}
+    	cout<<endl;
+    return moda[elemento];
             }
-int main() {
-	vector<string> moda;
-    mod(moda);
+int main(){
+	int tam;
+	int c;
+	vector<int> moda;
+    srand(time(NULL));
+	cin>>tam;
+	for(int i=0;i<tam+1;i++){
+		c=rand()%12 -1;
+		moda.push_back(c);
+	}
+    c=mod(moda);
+    cout<<c<<endl;
 	return 0;
 }
