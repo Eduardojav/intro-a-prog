81765daeb24bcb710de67339010b40c27fb4cc81
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-11-02 13:28:04 -0500

    Create cod2.cpp

---
+++ Lab03/cod2.cpp
@@ -0,0 +1,18 @@
+# incluye  < iostream >
+
+utilizando  namespace  std ;
+
+int  área ( int longitud, int ancho);
+
+int  main ()
+{
+    int x0 = área ( 7 , 8 );
+    int x1 = área ( 7 , 8 );
+    int x2 = área ( 7 , 2 );
+     área de retorno ( 4 , 4 );
+}
+
+int  área ( int longitud, int ancho)
+{
+    longitud de retorno * ancho;
+}
