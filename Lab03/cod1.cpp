d269eb7be947fd63c6f16f78ea3f9d2339ad04d7
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-11-02 13:27:33 -0500

    Create cod1.cpp

---
+++ Lab03/cod1.cpp
@@ -0,0 +1,19 @@
+# incluye  < iostream >
+
+utilizando  namespace  std ;
+
+int  área ( int longitud, int ancho);
+
+int  main ()
+{
+    int s1 = área ( 7 , 1 );
+    int s2 = área ( 7 , 6 );
+    int s3 = área ( 7 , 9 );
+    int s4 = área ( 7 , 4 );
+     área de retorno ( 4 , 4 );
+}
+
+int  área ( int longitud, int ancho)
+{
+    longitud de retorno * ancho;
+}
