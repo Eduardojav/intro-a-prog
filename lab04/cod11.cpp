8c639819e722d5a53f7dababef57cf91ac696d9f
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-11-11 12:59:28 -0500

    Create cod11.cpp

---
+++ lab04/cod11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+typedef int T;
+typedef double V;
+T f()
+{
+    V v;
+    return v;
+}
+
+double my_abs(int x)
+{
+    if(x<0)
+        return -x;
+    else if(x>0)
+        return x;
+}
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
