27851c6c80a6268462cb94b9caeb71783db75718
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-11-11 13:00:10 -0500

    Create cod13.cpp

---
+++ lab04/cod13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print_until(const vector<string>v, const string quit)
+{
+    for(int i=0; i<v.size();++i){
+        if(v[i]==quit)return;
+        cout<<v[i]<<'\n';
+    }
+}
+
+int main()
+{
+    return 0;
+}
