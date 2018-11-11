db1d4196d33b308449275d35c46991c841c59cc2
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-11-11 13:01:13 -0500

    Create cod15.cpp

---
+++ lab04/cod15.cpp
@@ -0,0 +1,23 @@
+include <iostream>
+#include <vector>
+using namespace std;
+
+void init(vector<double>& v)
+{
+    for(int i=0;i<v.size();++i)v[i]=i;
+}
+void g(int x)
+{
+    vector<double>vd1(10);
+    vector<double>vd2(1000000);
+    vector<double>vd3(x);
+    init(vd1);
+    init(vd2);
+    init(vd3);
+}
+
+int main()
+{
+    g(10);
+    return 0;
+}
