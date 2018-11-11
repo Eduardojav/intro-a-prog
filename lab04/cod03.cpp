13fa2d45eb86267e7ed22bc88e2ec53c7675fa48
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-11-11 12:55:46 -0500

    Create cod03.cpp

---
+++ lab04/cod03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int);
+
+int main()
+{
+    int i=7;
+    cout << f(i)<< endl;
+    return 0;
+}
+ double my_sqrt(double);
+ extern int a;
+ int a;
+ int x=7;
+ extern int x;
+ extern int x;
+
+ double my_sqrt(double);
+ double my_sqrt(double d){return 0.0;}
+ double my_sqrt(double);
+ double my_sqrt(double);
+
+ double expression();
+ double primary(){return expression();}
+ double term(){return primary();}
+ double expression(){return term();}
+ int f(int n){ return n;}
