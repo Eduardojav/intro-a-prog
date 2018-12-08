03ac341bcdfd5164f976d521b7fdc1b7b8a14b68
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2018-12-08 10:54:23 -0500

    Update cod07.cpp

--- lab06/cod07.cpp
+++ lab06/cod07.cpp
@@ -1,104 +1,127 @@
 #include<iostream>
-#include<string>
-#include<vector>
-#include<stdexcept>
-
-
+#include<stdlib.h>
+#include<string.h>
 using namespace std;
-
-class Anime{
-  public:
-    enum Geners{
-      Gore,Spokon,Romakone,Meitantei,Mecha,Shojo,MahoShojo,Gekiga,MagicalGirlfriend,Josei,Kemono,Kodomo,Cyberpunk,Shonen,Seinen,Sentai,comedia,Postapocaliptico
-    }; 
-    enum Formats{
-      serie,OVA,pelicula,manga
+    enum Marca {
+        TOYOTA=1,SUZUKI,CHERVOLET,AUDI,KIA,HYUNDAI};
+    enum Motor{
+        b1,b2,b3};
+    enum Color{ BLANCO=1, NEGRO,ROJO, AZUL, VERDE, GRIS ,PLOMO};
+Marca operator ++(Marca tipo){
+        if (tipo==HYUNDAI)
+            tipo=TOYOTA;
+        else
+            tipo=Marca(tipo+1);
+        return tipo ;}
+ ostream& operator << (ostream &o,const Marca &p)
+{   static const char* Marca_tbl[6]={"TOYOTA","SUZUKI","CHERVOLET","AUDI","KIA","HYUNDAI"
     };
+    return o << Marca_tbl[p-1];
+}
 
-    Anime(string _nombre,string _autor,vector<Geners>& alm_gen,Formats _formato, int _anho);
-
-
-    //  Operaciones que no se modificaran
-    string a() const {return nombre;}
-    string b() const {return autor;}
-    Geners c() const {return genero[0];}
-    Formats d() const {return formato;}
-    int e()    const {return anho;}
-	//void c()  { alm_gen[0]=genero[0];  alm_gen[1]=genero[1];}
-  private:
-    string  nombre,autor;
-    int     anho;
-    Formats formato;
-    vector<Geners> genero;
-
+ostream& operator << (ostream &o,const Color &p)
+{   static const char* Marca_tbl[7]={"BLANCO", "NEGRO","ROJO", "AZUL", "VERDE", "GRIS" ,"PLOMO"};
+    return o << Marca_tbl[p-1];
+}
+class Auto{
+private:
+    Marca marca;
+    Color color;
+    string motor;
+    string placa;
+    bool SOAT,energy,tecni;
+    int ano,vin,serie;
+    double gasolina,km=0;
+protected:
+public:
+    Marca in_marca(int x){
+    if(x<TOYOTA || x>HYUNDAI) error("marca no existente");
+    return Marca(x);}
+    Auto(int);
+    Auto(string,Marca,string,Color ,bool ,bool ,bool ,int , int ,int ,double ,double );
+    void error(string);
+    void salir();
+    void pasear();
+    void descrpg();
+    void empresa();
+    void legal();
+    void encender();
 };
-
-//Constructor de parametros y validacion
-Anime::Anime(string _nombre,string _autor, vector<Geners>& alm_gen,Formats _formato, int _anho){
-  if(_nombre=="0"){
-    _nombre="Desconocido";
-  }
-  if(_autor=="0"){
-    _autor="Desconocido";
-  }
-  for(int i=0;i<alm_gen.size();i++){
-  if( alm_gen[i]<Gore || alm_gen[i]>Postapocaliptico){ throw runtime_error("No conosco dicho genero");}
-  }
-  if( _formato<serie || _formato>manga){
-    throw runtime_error("No conosco dicho formato");
-  }
-  if(_anho<1917 || _anho>2018 ){
-    throw runtime_error("Anho de publicacion invalida");
-  }
-  nombre=_nombre;
-  autor=_autor;
-  genero=alm_gen;
-  formato=_formato;
-  anho=_anho;
-}
- 
- ostream& operator<<(ostream& os,const Anime::Geners& d){
-    static const char* genrs_tbl[18]={
-    "Gore","Spokon","Romakone","Meitantei","Mecha","Shojo","MahoShojo","Gekiga","MagicalGirlfriend","Josei","Kemono","Kodomo","Cyberpunk","Shonen","Seinen","Sentai","comedia","Postapocaliptico"
-  };
-  return os<<genrs_tbl[d];
- }
-
- ostream& operator<<(ostream& os,const Anime::Formats& d){
- 	static const char* form_tbl[4]={
-	 	"serie","OVA","pelicula","manga"
-	 };
-	 return os<<form_tbl[d];
- }
-int main()
-try{
-string nombre,autor;
-//  Anime genero;
-//  Anime formato;
-int a=0,anho=0;
-//Investigar como ingresar objetos nuevos creado en la class
-
-while(a==0){
-	cout<<"Escriba en orden: el nombre, autor, anho, formato y genero"<<endl;
-	cin>>nombre>>autor>>anho;
-	vector<Anime::Geners> alm_gen; 
-		alm_gen.push_back(Anime::Shonen);
-		alm_gen.push_back(Anime::Mecha);
-	
-	Anime p1(nombre,autor,alm_gen,Anime::OVA,anho);
-
-	cout<<"El nombre es: "<<p1.a()<<"\n";
-	cout<<"El autor es: "<<p1.b()<<"\n";
-	cout<<"Los generos son: "<<alm_gen[0]<<" "<<alm_gen[1]<<"\n";
-	cout<<"El formato es: "<<p1.d()<<"\n";
-	//cout<<"El genero es: "<<p1.c()<<"\n";
-	//cout<<"El formato es: "<<p1.d()<<"\n";
-	cout<<"El anho es: "<<p1.e()<<"\n";
-cout<<"\n";
+Auto::Auto(int){
+        placa="sda";
+        marca=TOYOTA;
+        motor="b1";
+        color=BLANCO;
+        SOAT=true;
+        energy=false;
+        km=0;
+        tecni=true;
+        ano=123;
+        vin=3241;
+        serie=123123;
 }
 
+Auto::Auto(string _placa, Marca _marca,string _motor,Color _color,bool _soat,bool _ener,bool _tecni,int _serie, int _vin,int _ano,double gasolina,double kilometraje=0){
+        placa=_placa;
+        marca=_marca;
+        motor=_motor;
+        color=_color;
+        SOAT=_soat;
+        energy=_ener;
+        km=kilometraje;
+        tecni=_tecni;
+        ano=_ano;
+        vin=_vin;
+        serie=_serie;}
+void Auto::error(string x){
+    cout<<x;}
+void Auto::salir(){
+    if(energy==true && gasolina>=10 && tecni==true){
+        cout<<"el auto puede correr"<<endl;
+        return;}
+    cout<<"el auto no puede o debe salir"<<endl;
+    }
+void Auto::descrpg(){
+    cout<<"es de la marca: "<<marca<<endl;
+    cout<<"n° de serie:"<<serie<<endl;
+    cout<<"numero VIN: "<<vin<<endl;
+    cout<<"es de color: "<<color<<endl;
+    cout<<"fabricado el año"<<ano<<endl;
+    cout<<"VIN: "<<vin<<endl;
+    cout<<"serie de fabricante: "<<serie<<endl;
+    cout<<"Estado de SOAT: "<<SOAT<<endl;
+    }
+void Auto::legal(){
+    if(SOAT==true && tecni==true)
+    cout<<"este auto si es legal para conducir"<<endl;
+else{
+    cout<<"sera multado si conduce este auto"<<endl;}
 }
-catch(exception & e){
-  cerr<<"Error: "<<e.what()<<"\n";
-  return 1;
+void Auto::encender(){
+if(energy== true && gasolina>=2 && tecni==true)
+        cout<<"usted encendio el auto"<<endl;
+else
+        cout<<"no puede encender el auto"<<endl;
+    }
+void Auto::pasear(){
+if(SOAT==true&& energy==true&& gasolina>=10&& tecni==true)
+    cout<<"usted puede pasear en el auto"<<endl;
+else{
+    cout<<"usted no puede pasear en este vehiculo"<<endl;
+}
+}
+
+int main(){
+
+    Marca mar = TOYOTA;
+    cout<< mar<<" "<<++mar;
+    Auto primo(12);
+    Auto primo1("VH6737H",TOYOTA,"P539PAS",BLANCO,true,false,true,125871,154785,1995,56.45);
+    primo.descrpg();
+    /*primo1.descrpg();
+    primo1. salir();
+    primo1. legal();
+    primo1. encender();
+    primo1. pasear();*/
+return 0;
 }
